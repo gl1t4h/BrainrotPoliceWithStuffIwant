cc314065852589b5ee7628bd60f72034ffbf26eb
gl1t4h <shyewilliams29@gmail.com>
2026-07-07 00:48:08 -0400

    Implement error notification in game script
    Added error handling message for script loading issues.

--- src/games/106211439579076.lua
+++ src/games/106211439579076.lua
@@ -1,1 +1,17 @@
 
+-- When the script gets 
+
+return function(section, data)
+    local elements = loadstring(game:HttpGet(getgitpath("src").."elements.lua"))()
+
+    local setdata = data[tostring(game.PlaceId)] or {}
+    data[tostring(game.PlaceId)] = setdata
+    writefile("BrainrotPolice/Config.json", game:GetService("HttpService"):JSONEncode(data))
+
+    local repStorage = game:GetService("ReplicatedStorage")
+    local plr = game:GetService("Players").LocalPlayer
+
+   elements:Label("You have been sent here because the script had an error when u loaded it. Try and load it again, if that doesn't work then wait while I fix it.", section)
+  
+    end)
+end
